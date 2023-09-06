c7d7201813d9c4a79c83bcdf24740859a10ae309
Nisal Bandara <nisalb@hey.com>
2023-09-06 17:55:19 +0530

    generics: duplicated code, unable to capture since the types are different

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,14 +1,26 @@
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
-    let result = largest(&number_list);
-    println!("largest = {result}");
+    let result = largest_i32(&number_list);
+    println!("largest i32 = {result}");
 
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-    let result = largest(&number_list);
-    println!("largest = {result}");
+    let number_list = vec!['y', 'm', 'a', 'q'];
+    let result = largest_char(&number_list);
+    println!("largest char = {result}");
 }
 
-fn largest(list: &[i32]) -> &i32 {
+fn largest_i32(list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
+
+    for number in list {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    largest
+}
+
+fn largest_char(list: &[char]) -> &char {
     let mut largest = &list[0];
 
     for number in list {
